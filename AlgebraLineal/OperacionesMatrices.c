f87f8463d8b1efdd4a7be57d7636b52e3be0b0ce
martin <marthofn@gmail.com>
2022-05-30 16:53:06 -0300

    Crear librería de operaciones básicas para matrices

--- AlgebraLineal/OperacionesMatrices.c
+++ AlgebraLineal/OperacionesMatrices.c
@@ -1,5 +1,33 @@
-//
-// Created by martin on 30/05/22.
-//
-
 #include "OperacionesMatrices.h"
+
+Matriz *multiplicacion_simple(Matriz *m1, Matriz *m2) {
+    return NULL;
+}
+
+Matriz *sumar(Matriz *m1, Matriz *m2) {
+    return NULL;
+}
+
+Matriz *restar(Matriz *m1, Matriz *m2) {
+    return NULL;
+}
+
+Matriz *multiplicar(Matriz *m1, Matriz *m2) {
+    return NULL;
+}
+
+Matriz *aplicar(double (*func)(double), Matriz *m) {
+    return NULL;
+}
+
+Matriz *escalar(double n, Matriz *m) {
+    return NULL;
+}
+
+Matriz *sumar_escalar(double n, Matriz *m) {
+    return NULL;
+}
+
+Matriz *transponer(Matriz *m) {
+    return NULL;
+}
--- AlgebraLineal/OperacionesMatrices.h
+++ AlgebraLineal/OperacionesMatrices.h
@@ -1,4 +1,18 @@
+#pragma once
+
+#include "Matriz.h"
+
 #ifndef PRIMERAREDNEURONAL_OPERACIONESMATRICES_H
 #define PRIMERAREDNEURONAL_OPERACIONESMATRICES_H
+
+Matriz *multiplicacion_simple (Matriz *m1, Matriz *m2);
+Matriz *sumar (Matriz *m1, Matriz *m2);
+Matriz *restar (Matriz *m1, Matriz *m2);
+Matriz *multiplicar (Matriz *m1, Matriz *m2);
+Matriz *aplicar (double (*func) (double ),Matriz *m);
+Matriz *escalar (double n, Matriz *m);
+Matriz *sumar_escalar (double n, Matriz *m);
+Matriz *transponer (Matriz *m);
+
 
 #endif //PRIMERAREDNEURONAL_OPERACIONESMATRICES_H
